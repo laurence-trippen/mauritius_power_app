31e27c6df16720c4a481e025a39510cdb000f0ae
Laurence Trippen <laurence.trippen@smartelephantapps.com>
2024-02-18 11:38:04 +0400

    added should know all district test

--- test/data/district_name_test.dart
+++ test/data/district_name_test.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mauritius_power_app/models/district.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  test("district enum should know strings", () {
+  test("getDistrictByString should return correct enum value", () {
     District blackriver = getDistrictByString('blackriver');
     District flacq = getDistrictByString('flacq');
     District grandport = getDistrictByString('grandport');
@@ -28,5 +31,37 @@ void main() {
     expect(rodrigues, equals(District.rodrigues));
 
     expect(unknown, equals(District.unknown));
+  });
+
+  test("district enum should know all districts", () async {
+    // ARRANGE
+    final testUri = Uri.https(
+      "raw.githubusercontent.com",
+      "MrSunshyne/mauritius-dataset-electricity/main/data/power-outages.json",
+    );
+
+    // ACT
+    final response = await http.get(testUri);
+
+    final Map<String, dynamic> decodedJson = json.decode(response.body);
+
+    final List<String> decodedDistrictStrings = decodedJson.keys.toList();
+
+    final districts = decodedDistrictStrings.map((districtName) {
+      // ignore: avoid_print
+      print("Found: $districtName");
+
+      District district = getDistrictByString(districtName);
+
+      if (district == District.unknown) {
+        // ignore: avoid_print
+        print("Found Unknown district: $districtName");
+      }
+
+      return district;
+    }).toList();
+
+    // ASSERT
+    expect(false, districts.contains(District.unknown));
   });
 }

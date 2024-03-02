c498d24631242805a9acdfe9510d2e12fddc0de1
Laurence Trippen <laurence.trippen@smartelephantapps.com>
2024-03-02 17:41:44 +0400

    date_validation_test wip

--- test/data/data_validation_test.dart
+++ test/data/data_validation_test.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
-import 'package:json_schema/json_schema.dart';
 
 void main() {
   test("dataset must be available", () async {
@@ -34,16 +32,109 @@ void main() {
     // ACT
     final response = await http.get(testUri);
 
-    File schemaFile = File("assets/schemas/dataset.schema.json");
-    final jsonSchemaString = await schemaFile.readAsString();
+    final Map<dynamic, dynamic> decodedJson = json.decode(response.body);
 
-    final schema = JsonSchema.create(jsonSchemaString);
-    final result = schema.validate(response.body);
+    // Check top-level object keys
+    bool checkAllKeysAreStrings(Map<dynamic, dynamic> root) {
+      for (final key in root.keys) {
+        final bool keyIsValid = key is String && key.isNotEmpty;
 
-    print(result.errors);
-    print(result.warnings);
+        if (!keyIsValid) {
+          print("Found invalid key: $key");
+          return false;
+        } else {
+          print("Key is ok: $key");
+        }
+      }
+
+      return true;
+    }
+
+    final bool allTopLevelKeysAreStrings = checkAllKeysAreStrings(decodedJson);
+
+    // Check district array of objects
+    bool validateRecordKeys(Map<dynamic, dynamic> json) {
+      for (final key in json.keys) {
+        if (key is String == false) {
+          print("Found invalid key (not a string): $key");
+          return false;
+        }
+
+        switch (key as String) {
+          case "date":
+          case "locality":
+          case "streets":
+          case "district":
+          case "from":
+          case "to":
+          case "id":
+            print("Valid record key: $key");
+          default:
+            print("Found invalid key: $key");
+            return false;
+        }
+      }
+
+      return true;
+    }
+
+    bool validateRecordValues(Map<dynamic, dynamic> json) {
+      for (final entry in json.entries) {
+        if (entry.value is String == false) {
+          print("Found invalid value (not a string): ${entry.value}");
+          return false;
+        }
+
+        switch (entry.key as String) {
+          case "date":
+          case "locality":
+          case "streets":
+          case "district":
+          case "id":
+            {
+              if ((entry.value as String).isEmpty) {
+                print("Found empty value: ${entry.key} => ${entry.value}");
+                return false;
+              }
+            }
+          case "from":
+          case "to":
+            {
+              try {
+                DateTime.tryParse(entry.value as String);
+              } catch (e) {
+                print(
+                    "Found invalid date format: ${entry.key} => ${entry.value}");
+                return false;
+              }
+            }
+          default:
+            print("Found invalid key: ${entry.key}");
+            return false;
+        }
+      }
+
+      return true;
+    }
+
+    final Map<dynamic, dynamic> testRecord = json.decode("""
+    {
+      "date": "Le mercredi 16 mars 2022 de  12:30:00 à  14:30:00",
+      "locality": "FLIC-EN-FLAC",
+      "streets": "MORC BISMIC",
+      "district": "blackriver",
+      "from": "2022-03-16T08:30:00.000Z",
+      "to": "2022-03-16T10:30:00.000Z",
+      "id": "f305aef9622b04c6b8aad5ffa847215e"
+    }
+    """);
+
+    bool areRecordKeysValid = validateRecordKeys(testRecord);
+    bool areRecordValuesValid = validateRecordValues(testRecord);
 
     // ASSERT
-    expect(result.isValid, equals(true));
+    expect(allTopLevelKeysAreStrings, equals(true));
+    expect(areRecordKeysValid, equals(true));
+    expect(areRecordValuesValid, equals(true));
   });
 }
